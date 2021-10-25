4c899796786f5930f6f17a0f2d127382a6e09107
fdavalo <57140104+fdavalo@users.noreply.github.com>
2021-10-25 21:40:48 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
+from pprint import pprint
+from hpeOneView.oneview_client import OneViewClient
+import sys, os
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self, ct):
+        self.send_response(200)
+        self.send_header('Content-type', ct)
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path == "/assets.js":
+            self._set_response('text/javascript')
+            js = "var assets = "+json.dumps(assets())
+            self.wfile.write(js.encode('utf-8'))
+        elif self.path == "/index.html" or self.path == "/":
+            file = open("resources/index.html")
+            self._set_response('text/html')
+            self.wfile.write(file.read().encode('utf-8'))
+        elif self.path == "/index.css":
+            file = open("resources/index.css")
+            self._set_response('text/css')
+            self.wfile.write(file.read().encode('utf-8'))
+        else:
+            self._set_response('text/html')
+            self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        if self.path == "/index.html" or self.path == "/":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            if post_data.startswith("asset=") and post_data != "asset=":
+              serverName = post_data.split("=")[1]
+              print(serverName)
+            file = open("resources/index.html")
+            self._set_response('text/html')
+            self.wfile.write(file.read().encode('utf-8'))
+        else:
+            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+            post_data = self.rfile.read(content_length) # <--- Gets the data itself
+            self._set_response('text/html')
+            self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+def assets():
+  assets = {}
+  file=open('resources/asset.yaml')
+  yaml = file.read()
+  file.close()
+  oneview_client = OneViewClient.from_environment_variables()
+  server_profiles = oneview_client.server_profiles
+  server_hardwares = oneview_client.server_hardware
+  server_hardware_all = server_hardwares.get_all()
+  all_profiles = server_profiles.get_all()
+  for profile in all_profiles:
+    role = ''
+    if 'master' in profile['name']: role = 'master'
+    if 'worker' in profile['name']: role = 'worker'
+    asset = {'role':role, 'username':os.environ.get('ONEVIEWSDK_USERNAME', ''),'password':os.environ.get('ONEVIEWSDK_PASSWORD', '')}
+    for conn in profile['connectionSettings']['connections']:
+      if conn['name'] == "RedHat_MGMT":
+        asset['mac']=conn['mac']
+    for hard in server_hardware_all:
+      if hard['uri'] == profile['serverHardwareUri']:
+        asset['url']='ipmi://'+hard['mpHostInfo']['mpIpAddresses'][0]['address']
+    assets[profile['name']]=asset
+    if 'url' in asset and 'mac' in asset and 'role' in asset:
+      file=open('assets/'+profile['name']+'.yaml', 'w+')
+      str=yaml.replace('@name@', profile['name'])
+      for key in ['url', 'mac', 'role']:
+        str = str.replace('@'+key+'@', asset[key])
+      file.write(str)
+      file.close()
+  pprint(assets)
+  return assets
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
